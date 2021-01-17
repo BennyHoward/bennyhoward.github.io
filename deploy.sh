95784be6cb8cab317185c7d329ca8fa6ab1cfde4
Benny Howard <BennyHoward@gmail.com>
2021-01-17 12:10:41 -0500

    Update deploy.sh
    Added check to only deploy from main branch.
    Added some formatting to the outputs.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/sh
 
+CURRENT_BRANCH=$(git branch --show-current)
+
+if [[ $CURRENT_BRANCH != 'main' ]]; then
+  echo "Error: Cannot deploy from branch '$CURRENT_BRANCH'.  Can ONLY deploy from 'main'."
+  exit 1
+fi
+
+echo 'Deploying to Github Pages.........'
+echo ''
+echo ''
+
 # Check for the LICENSE file and copy it if it doesn't exist, else just copy it.
 if [[ -f ./dist/LICENSE ]]; then
   echo 'LICENSE file found in deployment folder, ./dist.  Overwritting...'
@@ -9,6 +20,9 @@ else
   echo 'No LICENSE file found in deployment folder, ./dist.  Copying...'
   cp LICENSE ./dist/LICENSE
 fi
+echo ''
 
+echo "Deploying recent commits from branch 'main' to subtree branch 'gh-pages'...."
 # Push the contents of the ./dist folder to the gh-pages branch for deployment
 git subtree push --prefix dist origin gh-pages
+echo ''
